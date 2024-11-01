2dfabb89e1fc37e35fd652b7754dec652e070eaa
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-11-01 00:07:49 -0300

    teste

--- src/app.ts
+++ src/app.ts
@@ -23,5 +23,5 @@ app.setErrorHandler((error, request, reply) => {
     })
   }
 
-  return reply.status(500).send({ message: 'Internal server error' })
+  return reply.status(500).send({ message: error.message })
 })
